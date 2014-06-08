42b90c12459a949185e96cedb466c3ce5dfe9e8f
AliEn707 <1alien707@qip.ru>
2014-06-08 19:07:24 +0400

    multiple attack
    added multiple attack from bullet

--- engine_bullet.c
+++ engine_bullet.c
@@ -28,15 +28,18 @@ void tickProcessBullet(gnode * grid,bullet * b){
 		//vec dir={0,0};
 //		printf("!!%g %g\n",b->position.x,b->position.y);
 		//float length=getDir(&b->position,&b->destination,&dir);
+		float delta;
 		if (config.bullet_types[b->type].move_type!=SHOT){
 			b->position.x+=b->direction.x*config.bullet_types[b->type].speed;
 			b->position.y+=b->direction.y*config.bullet_types[b->type].speed;
+			delta=config.bullet_types[b->type].speed;
 		}else{
 			b->position.x=b->destination.x;
 			b->position.y=b->destination.y;
+			delta=0.1;
 		}
-		if (eqInD(b->position.x,b->destination.x,0.05)&&
-			eqInD(b->position.y,b->destination.y,0.05)){
+		if (eqInD(b->position.x,b->destination.x,delta)&&
+			eqInD(b->position.y,b->destination.y,delta)){
 			int i,j;
 			int multiple=0;
 			if (b->target==TOWER){
@@ -48,24 +51,35 @@ void tickProcessBullet(gnode * grid,bullet * b){
 						else
 							tmp->health-=b->damage;
 						multiple++;
-						printf("%d %d \n",tmp->type==BASE,config.players[tmp->owner].base_health);
-						
 					}
+				
 			}
-			if (b->target==NPC){  //add type area
+			if (config.bullet_types[b->type].attack_type==SINGLE && multiple>0)
+				goto out;
+			if (b->target==NPC){  
 				npc* tmp;
 				for(j=0;j<MAX_PLAYERS;j++)
 					for(tmp=grid[to2d((int)b->position.x,(int)b->position.y)].npcs[j];
 						tmp!=0;tmp=tmp->next)
 							if (tmp->isfriend!=b->isfriend)
-								if (eqInD(tmp->position.x,b->position.x,0.05)&&
-									eqInD(tmp->position.y,b->position.y,0.05)){
+								if (eqInD(tmp->position.x,b->position.x,0.1)&&
+									eqInD(tmp->position.y,b->position.y,0.1)){
 									tmp->health-=b->damage;
+									multiple++;
 									if (config.bullet_types[b->type].attack_type==SINGLE)
-										break;
+										goto out;
+									if (config.bullet_types[b->type].attack_type==MULTIPLE && 
+										multiple>config.bullet_types[b->type].area)
+										goto out;
 								}
+				
 			}
-		
+			//add area damage to Npc
+			//add area damage to towers
+			//if (config.bullet_types[b->type].attack_type==AREA ||
+			// 	config.bullet_types[b->type].attack_type==AREA_FF)
+			
+out:
 			b->detonate++;
 		}
 	}
