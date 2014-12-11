ab74ac909d45c8b19dd0e4430ed4b63c7eba599b
AliEn707 <1alien707@qip.ru>
2014-12-11 13:17:33 +0300

    change manager
    !!not tested!!

--- main/manager.c
+++ main/manager.c
@@ -42,6 +42,8 @@ messages and commands must be described in this file or another
 
 #define MANAGER "manager.ini"
 
+#define sendData(sock,buf,need) send(sock,buf,need,MSG_NOSIGNAL)
+
 int stop = 0;
 
 int recvData(int sock, void * buf, int size){
@@ -138,51 +140,85 @@ void * manager(void * arg) {
 			if ((sock = accept(listener, NULL, NULL))<0)
 				perror("Failed to accept");
 			char msg_type;//TODO: maybe fix!
+			//TODO: client host == public check
 			if (fcntl(sock, F_SETFD, fcntl(sock, F_GETFD) | FD_CLOEXEC) == -1)
 				perror("Failed to set socket attributes");
 			if (recvData(sock, &msg_type, sizeof(msg_type)) <= 0) {
 				close(sock);
 				continue;
 			}
-//			printf("get msg %d\n",msg_type);
-			//TODO: check client auth
-			int room_data = 0;
-			if (recvData(sock, &room_data, sizeof(room_data)) <= 0) {
-				close(sock);
-				continue;
+			if (msg_type==99){// ascii 'c' 
+	//			printf("get msg %d\n",msg_type);
+				//TODO: check client auth
+//				if (err){
+//					msg_type='e';
+//					sendData(sock, &msg_type, sizeof(msg_type));
+//					close(sock);
+//				}
+				int room_data = 0;
+				if (recvData(sock, &room_data, sizeof(room_data)) <= 0) {
+					close(sock);
+					continue;
+				}
+	//			printf("get token %d\n",room_data);
+				sem_server.sem_op = -1;
+	//			printf("sem %d\n",semctl(sem_id,0,GETVAL));
+	//			perror("sem");
+				semop(sem_id, &sem_server, 1);
+				int i = 0, flag = -1;
+				for(; i < servnum; i++)
+					if (ports_info[i] == 0) {
+						ports_info[i] = -1;
+						flag = i+startport;
+						break; 
+					}
+				sem_server.sem_op = 1;
+				semop(sem_id, &sem_server, 1);
+				pid_t pid;
+				if (flag != -1) {
+					char port_arg[15], token_arg[15];
+					switch (pid = fork()) { //create child process
+						case -1:
+							perror("Failed to fork");
+							exit(1);//TODO: send error report
+						case 0: //child process
+							sprintf(port_arg, "%d", flag);
+							sprintf(token_arg, "%d", room_data);
+							printf("!!!  port  %d token %d\n",flag,room_data);
+							if (execlp("./server", "./server", "-port", port_arg, "-token", token_arg, NULL) < 0) {//if (execlp("/bin/ls", "ls", 0, 0) < 0) {
+								msg_type='e';
+								sendData(sock, &msg_type, sizeof(msg_type));
+								close(sock);
+							}
+							break;
+						default:
+							close(sock);											
+					}
+				}
 			}
-//			printf("get token %d\n",room_data);
-			sem_server.sem_op = -1;
-//			printf("sem %d\n",semctl(sem_id,0,GETVAL));
-//			perror("sem");
-			semop(sem_id, &sem_server, 1);
-			int i = 0, flag = -1;
-			for(; i < servnum; i++)
-				if (ports_info[i] == 0) {
-					ports_info[i] = -1;
-					flag = i+startport;
-					break; 
-				}
-			sem_server.sem_op = 1;
-			semop(sem_id, &sem_server, 1);
-			pid_t pid;
-			if (flag != -1) {
-				char port_arg[15], token_arg[15];
-				switch (pid = fork()) { //create child process
-					case -1:
-						perror("Failed to fork");
-						exit(1);//TODO: send error report
-					case 0: //child process
-						sprintf(port_arg, "%d", flag);
-						sprintf(token_arg, "%d", room_data);
-						printf("!!!  port  %d token %d\n",flag,room_data);
-						if (execlp("./server", "./server", "-port", port_arg, "-token", token_arg, NULL) < 0) {//if (execlp("/bin/ls", "ls", 0, 0) < 0) {
-							close(sock);
-						}
-						break;
-					default:
-						close(sock);											
-				}
+			if (msg_type==115){// ascii 's' 
+				sem_server.sem_op = -1;
+				semop(sem_id, &sem_server, 1);
+				int i = 0, flag = -1;
+				for(; i < servnum; i++)
+					if (ports_info[i] != 0) {
+						flag++;
+					}
+				sem_server.sem_op = 1;
+				semop(sem_id, &sem_server, 1);
+				//send num of free servers
+				sendData(sock, &flag, sizeof(flag));//int
+				close(sock);
+			}
+			if (msg_type==105){// ascii 'i' 
+				sendData(sock, &menport, sizeof(menport));//int
+				sendData(sock, &servnum, sizeof(servnum));//int
+				sendData(sock, &startport, sizeof(startport));//int
+				close(sock);
+			}
+			if (msg_type==114){// ascii 'r' 
+			}
+			if (msg_type==117){// ascii 'u' 
 			}
 		}
 		syncTPS(timePassed(&tv),TPS);
